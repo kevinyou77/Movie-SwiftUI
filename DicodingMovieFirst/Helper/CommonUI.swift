9cd07cccb72ebc8367bf63887542369744fab6d8
Kevin Yulias <kevinyou77@gmail.com>
2020-08-31 19:03:20 +0700

    favorite games view bugfix

---
+++ DicodingMovieFirst/Helper/CommonUI.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftUI+EmptyState.swift
+//  DicodingMovieFirst
+//
+//  Created by Kevin Yulias on 25/08/20.
+//  Copyright © 2020 Kevin Yulias. All rights reserved.
+//
+
+import SwiftUI
+
+class CommonUI {
+	
+	static func EmptyState(text: String) -> some View {
+		Text(text)
+	}
+}
